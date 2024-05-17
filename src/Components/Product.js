c598f0d494044127236bb7d2ca77f4aa46d45217
Laxmi Aade <laxmiaade29@gmail.com>
2024-05-17 21:27:07 +0530

    ecomm commit

---
+++ src/Components/Product.js
@@ -0,0 +1,27 @@
+
+import React from 'react'
+import Card from './Card'
+import '../Css/Components/Product.css'
+
+const Product = ({search,handleClick,data}) => {
+
+  return (
+    <div className='Product'>
+      
+      <header className='pheader' >
+     
+      </header>
+
+
+       <section className="sec">
+       {data.filter((item)=>item.category.toLocaleLowerCase().includes(search)).map((item)=>{
+        return(
+          <Card key={item.id} item={item} handleClick={handleClick} />
+        )
+      })}    
+       </section> 
+    </div>
+  )
+}
+
+export default Product
